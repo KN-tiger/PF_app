54566c95e6d01a530cd695f9cc624e4cbd50716f
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-09-09 11:39:00 +0000

    [add]タグ一覧画面

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,2 +1,40 @@
 class Admin::TagsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @tags = Tag.all
+    @tag_new = Tag.new
+  end
+
+  def create
+    @tag_new = Tag.new(tag_params)
+    if @tag_new.save
+      redirect_to admin_tags_path
+      flash[:notice] = "タグ名を追加しました。"
+    else
+      @tags = Tag.all
+      render :index
+    end
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to admin_tags_path
+      flash[:notice] = "編集した内容を保存しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
 end

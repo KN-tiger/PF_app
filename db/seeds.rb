c5028f6727539844aab8f7a913dba010c1ae39bb
KN-tiger <108563821+KN-tiger@users.noreply.github.com>
2022-09-22 13:22:29 +0000

    [uproad] 画像パス変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admins = Admin.create!(
+  [
+    {last_name: "さんぷる", first_name: "花子", last_name_kana: "サンプル", first_name_kana: "ハナコ", login_id: "hanako@sample", password: "admin1234", telephone_number: "08012345678"},
+    {last_name: "ゲスト", first_name: "さん", last_name_kana: "ゲスト", first_name_kana: "サン", login_id: "guest@example", password: "kahagmklsaygua", telephone_number: "0120888555"}
+  ]
+)
+
+
+User.create!(
+  [
+    {last_name: "ゲスト", first_name: "さん", last_name_kana: "ゲスト", first_name_kana: "サン", login_id: "guest@example", password: "kahagmklsaygua", telephone_number: "0120888555"},
+  ]
+)
